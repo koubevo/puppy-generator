bd6844635309f57abbb0d268af6a21038709d8f3
Vojtěch Koubek <vojtech.koubek@seznam.cz>
2026-01-24 18:49:00 +0100

    hotfix: change time to 9-12 for puppy generator

--- app/Services/TaskSchedulerService.php
+++ app/Services/TaskSchedulerService.php
@@ -16,16 +16,16 @@ class TaskSchedulerService
         );
         $now = now();
 
-        if (! $task->next_run_at || $task->next_run_at->isBefore($now->startOfDay())) {
+        if (!$task->next_run_at || $task->next_run_at->isBefore($now->startOfDay())) {
             $task->update([
-                'next_run_at' => now()->setHour(rand(9, 17))->setMinute(rand(0, 59)),
+                'next_run_at' => now()->setHour(rand(9, 12))->setMinute(rand(0, 59)),
             ]);
 
             return null;
         }
 
         $isTime = $now->greaterThanOrEqualTo($task->next_run_at);
-        $notSentYet = ! $task->last_run_at || ! $task->last_run_at->isToday();
+        $notSentYet = !$task->last_run_at || !$task->last_run_at->isToday();
 
         return ($isTime && $notSentYet) ? $task : null;
     }
@@ -34,9 +34,9 @@ class TaskSchedulerService
     {
         $allowedTasks = config('tasks.allowed', []);
 
-        if (! in_array($taskName, $allowedTasks, true)) {
+        if (!in_array($taskName, $allowedTasks, true)) {
             throw new InvalidArgumentException(
-                "Task '{$taskName}' is not allowed. Allowed tasks: ".implode(', ', $allowedTasks)
+                "Task '{$taskName}' is not allowed. Allowed tasks: " . implode(', ', $allowedTasks)
             );
         }
     }
